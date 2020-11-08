cc4348230088c58b2a6136de5710fbe6f13f9c63
Денис Алхазов <bnkhasle@gmail.com>
2020-11-08 15:52:48 +0400

    fsm

--- src/main/java/FSM/JSONDeserializer.java
+++ src/main/java/FSM/JSONDeserializer.java
@@ -1,2 +1,50 @@
-package FSM;public class JSONDeserializer {
+package FSM;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.*;
+
+public class JSONDeserializer{
+    public List<FSM> deserialize(JsonArray jsonArray, String fsmName, int priority) throws JsonProcessingException {
+        List<FSM> fsms = new ArrayList<FSM>();
+        for(var jsonElem : jsonArray){
+            JsonObject json = jsonElem.getAsJsonObject();
+            //System.out.println(json);
+            Set<String> start = new HashSet<>();
+            json.getAsJsonArray("start").forEach(elem -> start.add(elem.toString().replaceAll("\"", "")));
+            Set<String> finish = new HashSet<>();
+            json.getAsJsonArray("finish").forEach(elem -> finish.add(elem.toString().replaceAll("\"", "")));
+            Map<String, String> inputs = new HashMap<>();
+            if (json.get("inputs") != null) {
+                for(var input : json.get("inputs").getAsJsonObject().keySet()) {
+                    inputs.put(input, json.get("inputs").getAsJsonObject().get(input).toString());
+                }
+            }
+            Map<String, Map<String, List<String>>> jsonMatrix = new HashMap<>();
+            for (var q : json.get("matrix").getAsJsonObject().keySet()) {
+                Map<String, List<String>> map = new HashMap<>();
+                for(var input : json.get("matrix").getAsJsonObject().get(q).getAsJsonObject().keySet()){
+                    List<String> list = new ArrayList<>();
+                    json.get("matrix").getAsJsonObject().get(q).getAsJsonObject().get(input).getAsJsonArray()
+                            .forEach(elem-> list.add(elem.toString().replaceAll("\"","")));
+                    map.put(input, list);
+                }
+                jsonMatrix.put(q, map);
+            }
+            FSM fsm = new FSM();
+            fsm.setClassName(fsmName);
+            fsm.setPriority(priority);
+            fsm.setStartStates(start);
+            fsm.setEndStates(finish);
+            fsm.setAllStates(jsonMatrix.keySet());
+            fsm.setInputs(inputs);
+            fsm.setMatrix(jsonMatrix);
+            fsms.add(fsm);
+        }
+
+        return fsms;
+    }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,28 +1,90 @@
-package FSM;
-
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import FSM.FSM;
+import FSM.FSMGroup;
+import FSM.JSONDeserializer;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.apache.commons.jexl2.JexlContext;
+import org.apache.commons.jexl2.JexlEngine;
+import org.apache.commons.jexl2.MapContext;
+import util.Pair;
 
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
 import java.io.*;
-import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Program {
-    public static void main(String[] args) throws IOException, ParseException {
-        FileReader in = new FileReader("src/main/java/FSM/integer.json");
-        JsonReader jsonReader = new JsonReader(in);
-        JsonArray jsonArray = new JsonParser().parse(jsonReader).getAsJsonArray();
-        JSONDeserializer jsonDeserializer = new JSONDeserializer();
-        jsonDeserializer.deserialize(jsonArray);
+    public static String readFile(String path) throws IOException {
+        return new String(Files.readAllBytes(Path.of(path))).replace('\r', ' ')
+                .replace('\n', ' ').replaceAll("( )+", " ")
+                .replaceAll(";", " ;").replaceAll("\\(", "( ")
+                .replaceAll("\\)", " )");
+    }
+    public static void main(String[] args) throws IOException {
+        List<FSM> fsmList = new ArrayList<FSM>();
+        FSMGroup fsmGroup = new FSMGroup();
+        FileReader config = new FileReader("src/main/resources/config/config.json");
+        JsonReader jsonReader = new JsonReader(config);
+        JsonObject jsonObject = new JsonParser().parse(jsonReader).getAsJsonObject();
+        //the first task
+        for (var key : jsonObject.keySet()){
+            String jsonPath = jsonObject.get(key).getAsJsonObject().get("path").toString().replaceAll("\"", "");
+            int priority = Integer.parseInt(jsonObject.get(key).getAsJsonObject().get("priority").toString());
+            //System.out.println(jsonPath);
+            if (jsonPath.equals("src/main/resources/lexer/integer.json") || jsonPath.equals("src/main/resources/lexer/real.json")){
+                FileReader in = new FileReader(jsonPath);
+                jsonReader = new JsonReader(in);
+                JsonArray jsonArray = new JsonParser().parse(jsonReader).getAsJsonArray();
+                JSONDeserializer jsonDeserializer = new JSONDeserializer();
+                jsonDeserializer.deserialize(jsonArray, key, priority).forEach(fsm -> fsmList.add(fsm));
+            }
+        }
+        //fsmList.forEach(System.out::println);
+        fsmGroup.setFsms(fsmList);
+        System.out.println(fsmGroup.max(".", 0)); // 0, false
+        System.out.println(fsmGroup.max(".1", 0)); // 2, true
+        System.out.println(fsmGroup.max("123",0)); // 3, true
+        System.out.println(fsmGroup.max("a123", 0)); // 0, false
+        System.out.println(fsmGroup.max("123.",0)); // 4, true
+        System.out.println(fsmGroup.max("12.3.",0)); // 4, true
+        System.out.println(fsmGroup.max("123.1",0)); // 5, true
+        System.out.println(fsmGroup.max("123.123",0)); // 7, true
+        System.out.println(fsmGroup.max("+123.123",0)); // 8, true
+        System.out.println(fsmGroup.max("-123.123",0)); // 8, true
+        System.out.println(fsmGroup.max("a123.123",0)); // 0, false
+        System.out.println(fsmGroup.max("a123.123", 1)); // 7, true
+        System.out.println(fsmGroup.max("a123.123", 4)); // 4, true
+        System.out.println(fsmGroup.max("a123.123", 5)); // 3, true
 
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        //the second task
+        fsmList.clear();
+
+        for (var key : jsonObject.keySet()){
+            String jsonPath = jsonObject.get(key).getAsJsonObject().get("path").toString().replaceAll("\"", "");
+            int priority = Integer.parseInt(jsonObject.get(key).getAsJsonObject().get("priority").toString());
+            FileReader in = new FileReader(jsonPath);
+            jsonReader = new JsonReader(in);
+            JsonArray jsonArray = new JsonParser().parse(jsonReader).getAsJsonArray();
+            JSONDeserializer jsonDeserializer = new JSONDeserializer();
+            jsonDeserializer.deserialize(jsonArray, key, priority).forEach(fsm -> fsmList.add(fsm));
+        }
+        fsmGroup.setFsms(fsmList);
+        //fsmList.forEach(System.out::println);
+        System.out.println(readFile("src/main/resources/program.txt"));
+        String[] unrecognizedTokens = readFile("src/main/resources/program.txt").split(" ");
+        for (String unrecognizedToken : unrecognizedTokens) {
+            Pair<FSM, Pair<Integer, Boolean>> fsmPair = fsmGroup.defineTokens(unrecognizedToken, 0);
+            if (fsmPair!= null){
+                System.out.println("" + unrecognizedToken + " --> " + fsmPair.getKey().getClassName() + " priority = " + fsmPair.getKey().getPriority());
+            }
+        }
+        //System.out.println(fsmGroup.defineTokens("15", 0));
     }
 }
