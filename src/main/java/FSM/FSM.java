b8c9d39a0853462289ce343f5660872e541f6d7a
Денис Алхазов <bnkhasle@gmail.com>
2020-11-08 15:43:52 +0400

    fsm

--- src/main/java/FSM/FSM.java
+++ src/main/java/FSM/FSM.java
@@ -1,2 +1,157 @@
-package FSM;public class FSM {
+package FSM;
+
+import org.apache.commons.jexl2.JexlContext;
+import org.apache.commons.jexl2.JexlEngine;
+import org.apache.commons.jexl2.MapContext;
+import util.Pair;
+
+import java.util.*;
+
+public class FSM {
+    Set<String> startStates;
+    Set<String> endStates;
+    String currentState = "q0";
+    Set<String> allStates = new HashSet<>();
+    Map<String, String> inputs = new HashMap<>();
+    Map<String, Map<String, List<String>>> matrix = new HashMap<>();
+    String className = "";
+    int priority;
+
+    public Map<String, Map<String, List<String>>> getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(Map<String, Map<String, List<String>>> matrix) {
+        this.matrix = matrix;
+    }
+
+    public Map<String, String> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(Map<String, String> inputs) {
+        this.inputs = inputs;
+    }
+
+    public Set<String> getStartStates() {
+        return startStates;
+    }
+
+    public void setStartStates(Set<String> startStates) {
+        this.startStates = startStates;
+    }
+
+    public Set<String> getEndStates() {
+        return endStates;
+    }
+
+    public void setEndStates(Set<String> endStates) {
+        this.endStates = endStates;
+    }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(String currentState) {
+        this.currentState = currentState;
+    }
+
+    public Set<String> getAllStates() {
+        return allStates;
+    }
+
+    public void setAllStates(Set<String> allStates) {
+        this.allStates = allStates;
+    }
+
+    public FSM() {
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return  "class = " + className + "\npriority = " + priority + "\nstart: " + startStates + "\nfinish: " + endStates + "\ninputs: " + inputs + "\nmatrix: " + matrix + "\n";
+    }
+    public Pair<Integer, Boolean> max(String input, int skip) {
+        Pair<Integer, Boolean> pair = Pair.of(0, false);
+        for(int i = skip; i < input.length(); i++){
+            //System.out.println(input.charAt(i));
+            boolean isCorrect = feedFSM(input.charAt(i));
+            if (isCorrect){
+                pair = Pair.of(i + 1 - skip, true);
+            }
+            else {
+                break;
+            }
+        }
+        if (endStates.contains(currentState)){
+            currentState = "q0";
+            //System.out.println(pair);
+            return pair;
+        }
+        else {
+            currentState = "q0";
+            //System.out.println(pair);
+            return Pair.of(0,false);
+        }
+
+
+    }
+    public Boolean feedFSM(Character character){
+        //System.out.println(currentState);
+        String inputType = null;
+        String state = null;
+        if (!inputs.isEmpty()){
+            inputType = getInputType(character);
+        }
+        if (matrix.get(currentState) != null && matrix.get(currentState).get("" + character) != null){
+            currentState = matrix.get(currentState).get("" + character).get(0);
+        }
+        else if (inputType!= null && matrix.get(currentState) != null && matrix.get(currentState).get(inputType)!= null){
+            //System.out.println(matrix.get(currentState));
+            currentState = matrix.get(currentState).get(inputType).get(0);
+            //System.out.println(currentState);
+        }
+        else {
+            return false;
+        }
+        //System.out.println(currentState);
+        return true;
+
+    }
+
+    public String getInputType(Character character){
+        String outType = null;
+        for (var key : inputs.keySet()){
+            JexlEngine expressionFactory = new JexlEngine();
+            JexlContext context = new MapContext();
+            //System.out.println(inputs.get(key).replaceAll("'x'", "'" + character.toString() + "'").replaceAll("\"", ""));
+           /* System.out.println(inputs.get(key)
+                    .replaceAll("'x'", "'" + character.toString() + "'").replaceAll("\"", "").replaceAll("\n", " "));*/
+            org.apache.commons.jexl2.Expression expression = expressionFactory.createExpression(inputs.get(key)
+                    .replaceAll("'x'", "'" + character.toString() + "'").replaceAll("\"", "").replaceAll("\n", " "));
+            boolean result=(Boolean)expression.evaluate(context);
+            //System.out.println(result);
+            if (result)
+                outType = key;
+            //System.out.println(outType);
+        }
+        return outType;
+    }
 }
--- src/main/java/FSM/FSMGroup.java
+++ src/main/java/FSM/FSMGroup.java
@@ -1,2 +1,142 @@
-package FSM;public class FSMGroup {
+package FSM;
+
+import util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FSMGroup {
+    List<FSM> fsms = new ArrayList<FSM>();
+
+    public List<FSM> getFsms() {
+        return fsms;
+    }
+
+    public void setFsms(List<FSM> fsms) {
+        this.fsms = fsms;
+    }
+
+    public FSMGroup() {
+    }
+
+    /*public Pair<Integer, Boolean> max(String input, int skip){
+        List<Pair<Integer, Boolean>> results = new ArrayList<>();
+        Pair<Integer, Boolean> pairOfResult = Pair.of(0, false);
+        for (var fsm: fsms){
+            results.add(fsm.max(input, skip));
+        }
+        for (var result : results){
+            if (result.getKey() > pairOfResult.getKey()){
+                pairOfResult = Pair.of(result.getKey(), result.getValue());
+            }
+        }
+        return pairOfResult;
+    }*/
+    public Pair<Integer, Boolean> max(String input, int skip){
+        Pair<Integer, Boolean> result = Pair.of(0, false);
+        Map<FSM, Pair<Integer, Boolean>> results = new HashMap<FSM, Pair<Integer, Boolean>>();
+        Map<FSM, Boolean> success = new HashMap<FSM, Boolean>();
+        for (var fsm: fsms){
+            results.put(fsm, Pair.of(0, false));
+            success.put(fsm, true);
+        }
+        for(int i=skip; i<input.length(); i++) {
+            for (var fsm : fsms) {
+                if (success.get(fsm)){
+                    boolean isCorrect = fsm.feedFSM(input.charAt(i));
+                    if (isCorrect){
+                        results.put(fsm, Pair.of(i+1-skip, true));
+                    }
+                    else {
+                        success.put(fsm, false);
+                    }
+                }
+            }
+        }
+        for(var fsm: fsms){
+            if (fsm.getEndStates().contains(fsm.currentState)){
+                fsm.setCurrentState("q0");
+            }
+            else {
+                fsm.setCurrentState("q0");
+                results.put(fsm, Pair.of(0, false));
+            }
+        }
+        int priority = -1;
+        for (var fsm : results.keySet()){
+            //System.out.println("" + fsm.getClassName() + " " + results.get(fsm) + " fsmPriority = " + fsm.getPriority());
+            if (results.get(fsm) != null && fsm.getPriority() > priority && results.get(fsm).getKey() != 0){
+                //System.out.println(fsm);
+                priority = fsm.getPriority();
+                result = Pair.of(results.get(fsm).getKey(), results.get(fsm).getKey()!=0);
+                //System.out.println(fsm);
+            }
+            else if (results.get(fsm) != null && fsm.getPriority() == priority && results.get(fsm).getKey() > result.getKey()) {
+                priority = fsm.getPriority();
+                result = Pair.of(results.get(fsm).getKey(), results.get(fsm).getKey()!=0);
+            }
+            /*if (results.get(fsm).getKey() != 0){
+                System.out.println(fsm);
+            }*/
+        }
+
+
+        return result;
+    }
+
+    public Pair<FSM, Pair<Integer, Boolean>> defineTokens(String input, int skip){
+        Pair<FSM, Pair<Integer, Boolean>> fsmPair = null;
+        Pair<Integer, Boolean> result = Pair.of(0, false);
+        Map<FSM, Pair<Integer, Boolean>> results = new HashMap<FSM, Pair<Integer, Boolean>>();
+        Map<FSM, Boolean> success = new HashMap<FSM, Boolean>();
+        for (var fsm: fsms){
+            results.put(fsm, Pair.of(0, false));
+            success.put(fsm, true);
+        }
+        for(int i=skip; i<input.length(); i++) {
+            for (var fsm : fsms) {
+                if (success.get(fsm)){
+                    boolean isCorrect = fsm.feedFSM(input.charAt(i));
+                    if (isCorrect){
+                        results.put(fsm, Pair.of(i+1-skip, true));
+                    }
+                    else {
+                        success.put(fsm, false);
+                    }
+                }
+            }
+        }
+        for(var fsm: fsms){
+            if (fsm.getEndStates().contains(fsm.currentState)){
+                fsm.setCurrentState("q0");
+            }
+            else {
+                fsm.setCurrentState("q0");
+                results.put(fsm, Pair.of(0, false));
+            }
+        }
+        int priority = -1;
+        for (var fsm : results.keySet()){
+            //System.out.println("" + fsm.getClassName() + " " + results.get(fsm) + " fsmPriority = " + fsm.getPriority());
+            if (results.get(fsm) != null && fsm.getPriority() > priority && results.get(fsm).getKey() != 0){
+                //System.out.println(fsm);
+                priority = fsm.getPriority();
+                result = Pair.of(results.get(fsm).getKey(), results.get(fsm).getKey()!=0);
+                fsmPair = Pair.of(fsm, result);
+                //System.out.println(fsm);
+            }
+            else if (results.get(fsm) != null && fsm.getPriority() == priority && results.get(fsm).getKey() > result.getKey()) {
+                priority = fsm.getPriority();
+                result = Pair.of(results.get(fsm).getKey(), results.get(fsm).getKey()!=0);
+                fsmPair = Pair.of(fsm, result);
+            }
+            /*if (results.get(fsm).getKey() != 0){
+                System.out.println(fsm);
+            }*/
+        }
+        return fsmPair;
+
+    }
 }
